3aa6528017e8a0221b65551605500fe979b7a44e
igorhenri5 <razrplay5@gmail.com>
2016-09-17 18:34:51 -0300

    PROJETÃO DO FEIJÃO
    Base pra estrutura de MODEL, com exemplos de como deve ser feita, imagem
    do BD e todos os documentos do dropbox ( mais atualizados )

---
+++ Krift-core/src/model/service/GenericManter.java
@@ -0,0 +1,15 @@
+package model.service;
+
+import util.db.exception.NegocioException;
+import util.db.exception.PersistenciaException;
+import java.util.List;
+
+public interface GenericManter<Entidade> {
+    
+    public void cadastrar(Entidade e) throws PersistenciaException, NegocioException;
+    public void alterar(Entidade e) throws PersistenciaException, NegocioException;
+    public Entidade buscarPorId(Integer id) throws PersistenciaException, NegocioException;
+    public void excluir(Entidade e) throws PersistenciaException, NegocioException;
+    public List<Entidade> listarTodos() throws PersistenciaException, NegocioException;
+
+}
---
+++ Krift-core/src/util/db/JDBCConnectionFactory.java
@@ -0,0 +1,9 @@
+package util.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface JDBCConnectionFactory {
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException;
+}
